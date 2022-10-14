a8e588b356a750086ed1b1835944c80fded56f49
Dmitry Zhelnin <mitya_zh@mail.ru>
2022-10-14 13:15:07 +0300

    explicit viewmodel-view registrations

--- src/Quinta/ViewLocator.cs
+++ src/Quinta/ViewLocator.cs
@@ -7,9 +7,17 @@ namespace Quinta;
 
 public class ViewLocator : IDataTemplate
 {
+    private readonly Dictionary<Type, Type> _registry = new();
+
     public IControl Build(object data)
     {
-        var name = data.GetType().AssemblyQualifiedName!.Replace("ViewModel", "View");
+        var viewModelType = data.GetType();
+        if (_registry.TryGetValue(viewModelType, out var viewType))
+        {
+            return (Control)Activator.CreateInstance(viewType)!;
+        }
+
+        var name = viewModelType.AssemblyQualifiedName!.Replace("ViewModel", "View");
         var type = Type.GetType(name);
 
         if (type != null)
@@ -23,5 +31,10 @@ public class ViewLocator : IDataTemplate
     public bool Match(object data)
     {
         return data is ReactiveObject || data is IDockable;
+    }
+
+    public void Register<TViewModel, TView>() where TView : Control
+    {
+        _registry[typeof(TViewModel)] = typeof(TView);
     }
 }
